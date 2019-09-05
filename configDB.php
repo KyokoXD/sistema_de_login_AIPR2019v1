9e681dd90221d9b421859d55afd14bc220971b1d
KyokoXD <48030617+KyokoXD@users.noreply.github.com>
2019-09-05 13:21:44 -0300

    Update configDB.php

--- configDB.php
+++ configDB.php
@@ -1,16 +1,15 @@
 <?php
-//configDB.php
-
-//Dados para escolha do Data (DB)
+//configDB.php 
+//Dados para escolha do DataBase (DB)
 $dbhost = "localhost";
-$dbuser = "root";//Usúariso Raíz (Rute)
+$dbuser = "root"; //Usuário Raíz (Rute)
 $dbpass = "";
 $dbname = "sistemaDeLogin";
-
 //Conexão com o banco de dados
 $conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if($conecta->connect_error){
-    die("Não foi possivel conectar ao Banco de Dados: " . $conecta->connect_error);
+    die("Não foi possível conectar ao 
+    Banco de Dados: " . $conecta->connect_error);
 }else{
-    echo "<h1>Conectou no BD Manowwwww!</h1>";
-}    
+    //echo "<h1>Conectou no BD Manowwwww!</h1>";
+}
